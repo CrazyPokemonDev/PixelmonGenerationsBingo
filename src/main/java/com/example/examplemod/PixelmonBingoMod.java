9f9f2f242cd06d0946d4479223d277effec952f3
Olfi01 <flom@meyer-alpers.de>
2022-07-13 13:26:54 +0200

    Basic mod info

---
+++ src/main/java/com/example/examplemod/PixelmonBingoMod.java
@@ -0,0 +1,41 @@
+package com.example.examplemod;
+
+import com.lypaka.lypakautils.ConfigurationLoaders.BasicConfigManager;
+import com.lypaka.lypakautils.ConfigurationLoaders.ConfigUtils;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Mod(modid = PixelmonBingoMod.MODID, name = PixelmonBingoMod.NAME, version = PixelmonBingoMod.VERSION)
+public class PixelmonBingoMod
+{
+    public static final String MODID = "de.crazypokemondev.pixelmongenerations.bingo";
+    public static final String NAME = "Pixelmon Bingo";
+    public static final String VERSION = "0.0.1";
+    public static BasicConfigManager configManager;
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) throws IOException {
+        logger = event.getModLog();
+        Path dir = ConfigUtils.checkDir(Paths.get("./config/pixelmonbingo"));
+        String[] files = new String[]{"pixelmonbingo.conf"};
+        configManager = new BasicConfigManager(files, dir, PixelmonBingoMod.class, NAME, MODID, logger);
+        configManager.init();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
